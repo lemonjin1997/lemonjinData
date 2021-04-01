182758a4c43494c5bd1e50de0bf364d04b95cdcf
lemonjin1997 <53006700+lemonjin1997@users.noreply.github.com>
2021-04-02 00:05:01 +0800

    Update Driver.java Integration completed

--- Driver.java
+++ Driver.java
@@ -15,7 +15,9 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Driver {
 	
 	public static void main(String[] args) throws Exception {
+		
 		Configuration conflemonjin = new Configuration();
+		//lemonjin part's
 		Job joblemonjin = Job.getInstance(conflemonjin, "AirlineNegativeSentiments");
 		joblemonjin.setJarByClass(Driver.class);
 		Path COVID_Cases = new Path("hdfs://localhost:9000/user/phamvanvung/project/input/COVID-Cases.csv");
@@ -23,6 +25,7 @@ public class Driver {
 		Path STI_2021 = new Path("hdfs://localhost:9000/user/phamvanvung/project/input/STI_2021.csv");
 		Path outPath_lemonjin = new Path("hdfs://localhost:9000/user/phamvanvung/project/output/lemonjin");
 		outPath_lemonjin.getFileSystem(conflemonjin).delete(outPath_lemonjin, true);
+		
 		
 		/*Configuration validationConf = new Configuration(false);
 		ChainMapper.addMapper(job, ANSValidationMapper.class, LongWritable.class,
@@ -33,6 +36,7 @@ public class Driver {
 				Text.class, IntWritable.class, ansConf);*/
 		
 		//job.setMapperClass(ChainMapper.class);
+		
 		
 		MultipleInputs.addInputPath(joblemonjin, COVID_Cases, TextInputFormat.class, COVIDMapper.class);
 		MultipleInputs.addInputPath(joblemonjin, STI_2020, TextInputFormat.class, STIRateMapper.class);
@@ -47,7 +51,7 @@ public class Driver {
 		//FileInputFormat.addInputPath(job, inPath);
 		FileOutputFormat.setOutputPath(joblemonjin, outPath_lemonjin);
 		
-		
+		//BW's part
 		Configuration confBW = new Configuration();
 		  Job jobBW =Job.getInstance(confBW, "CovidvsGDP");
 		  Path Covid19Inp = new Path("hdfs://localhost:9000/user/phamvanvung/project/input/Covid-19-SG-2020.tsv");
@@ -69,9 +73,53 @@ public class Driver {
 		  outPathBW.getFileSystem(confBW).delete(outPathBW, true);
 		  FileOutputFormat.setOutputPath(jobBW, outPathBW);
 		  
+		  //LQSE's part
+		  Configuration confLQSE = new Configuration();
+			Job jobLQSE = Job.getInstance(confLQSE, "ProjDriver");
+			Path countryInput = new Path("hdfs://localhost:9000/user/phamvanvung/project/input/ISO-3166-alpha3.tsv");
+			Path inputPath = new Path("hdfs://localhost:9000/user/phamvanvung/project/input/WHO-COVID-19-global-data.csv");
+			
+			MultipleInputs.addInputPath(jobLQSE, countryInput, TextInputFormat.class, ProjMapperCountry.class);
+			MultipleInputs.addInputPath(jobLQSE, inputPath, TextInputFormat.class, ProjMapperMaxDeaths.class);
+			jobLQSE.setJarByClass(Driver.class);
+			
+			jobLQSE.setOutputKeyClass(Text.class);
+			jobLQSE.setOutputValueClass(Text.class);
+			jobLQSE.setMapOutputKeyClass(Text.class);
+			jobLQSE.setMapOutputValueClass(Text.class);
+			jobLQSE.setReducerClass(ProjReducerMax.class);
+			Path outputPath = new Path("hdfs://localhost:9000/user/phamvanvung/project/output/LQSE");
+			
+			FileInputFormat.addInputPath(jobLQSE, inputPath);
+			FileOutputFormat.setOutputPath(jobLQSE, outputPath);
+			outputPath.getFileSystem(confLQSE).delete(outputPath, true);
+			
+			
 		  jobBW.waitForCompletion(true);
 		  
 		  joblemonjin.waitForCompletion(true);
+		  jobLQSE.waitForCompletion(true);
+
+		  Configuration conf = new Configuration(); 
+	        Job job = Job.getInstance(conf, "top 10"); 
+	        job.setJarByClass(Driver.class); 
+	  
+	        job.setMapperClass(top10_Mapper.class); 
+	        job.setReducerClass(top10_Reducer.class); 
+	  
+	        job.setMapOutputKeyClass(Text.class); 
+	        job.setMapOutputValueClass(LongWritable.class); 
+	  
+	        job.setOutputKeyClass(LongWritable.class); 
+	        job.setOutputValueClass(Text.class); 
+	        Path inputPath1 = new Path("hdfs://localhost:9000/user/phamvanvung/project/output/LQSE/part-r-00000");
+			Path outputPath1 = new Path("hdfs://localhost:9000/user/phamvanvung/project/output/LQSETop10");
+			outputPath1.getFileSystem(conf).delete(outputPath1, true);
+			
+	        FileInputFormat.addInputPath(job, inputPath1); 
+	        FileOutputFormat.setOutputPath(job, outputPath1); 
+	        job.waitForCompletion(true);
+	        
 	}
 
 }
